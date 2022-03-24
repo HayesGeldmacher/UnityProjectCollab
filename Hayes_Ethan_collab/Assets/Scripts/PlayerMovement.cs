afc35c797a48fa7cc7567bf870af086396a96aa6
ethangilmore <ethanmckgilmore@gmail.com>
2022-03-23 17:18:20 -0700

    Updated player movement
    allowed rotation with mouse movement

--- Hayes_Ethan_collab/Assets/Scripts/PlayerMovement.cs
+++ Hayes_Ethan_collab/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // public stuff
+    [Header("Movement")]
+    public float ForwardSpeed = 10;
+    public float SidewaysSpeed = 5;
 
-    public float ForwardSpeed, SidewaysSpeed = 10;
-
+    // private stuff
     private float hInput, vInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -19,9 +22,6 @@ public class PlayerMovement : MonoBehaviour
     {
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
-
-
-        
 
         Vector3 direction = Vector3.forward*vInput*ForwardSpeed+Vector3.right*hInput*SidewaysSpeed;
         transform.Translate(direction*Time.deltaTime);
@@ -31,7 +31,9 @@ public class PlayerMovement : MonoBehaviour
 
     void UpdateRotation()
 	{
+        float mouseX = Input.GetAxis("Mouse X");
 		Vector3 gravityUp = transform.position.normalized;
 		transform.rotation = Quaternion.FromToRotation(transform.up, gravityUp) * transform.rotation;
+        transform.Rotate(Vector3.up, mouseX);
 	}
 }
