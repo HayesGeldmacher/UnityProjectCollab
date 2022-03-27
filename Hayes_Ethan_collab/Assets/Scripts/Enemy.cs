3b74a4b874b6908d0cc20efe21a9b09f6d0071fb
ethangilmore <ethanmckgilmore@gmail.com>
2022-03-26 18:05:02 -0600

    Created Basic Enemy Script

---
+++ Hayes_Ethan_collab/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform BulletSpawn;
+
+    [Header("Movement")]
+    public float MovementSpeed;
+    public float PlayerTargetRadius;
+
+    [Header("Attacks")]
+    public GameObject Projectile;
+    public float DespawnTime;
+    public float CooldownTime;
+
+    private float _cooldown;
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletSpawn.transform.LookAt(_player.transform);
+        _cooldown -= Time.deltaTime;
+        if (_cooldown <= 0)
+            FireProjectile();
+    }
+
+    void FireProjectile()
+    {
+        _cooldown += CooldownTime;
+        GameObject proj = Instantiate(Projectile, BulletSpawn);
+        proj.transform.SetParent(null);
+        Destroy(proj, DespawnTime);
+    }
+}
