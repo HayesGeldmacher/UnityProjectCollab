7337fbef6a834bbb4f06a87f90b929bd45310b79
ethangilmore <ethanmckgilmore@gmail.com>
2022-03-31 13:57:24 -0700

    Started Attack System/Component

---
+++ Hayes_Ethan_collab/Assets/Scripts/Attack.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public AttackSpawn[] AttackSpawns;
+    public AttackInfo[] Projectiles;
+    public AttackTrigger Triggers;
+
+
+    private Dictionary<string, AttackSpawn> _attackSpawnLookup;
+    private float[] _projectileCooldowns;
+    private int[] _timesFired;
+    void Start(){
+        InitLookup();
+        InitCooldowns();
+    }
+
+    void InitLookup(){
+        _attackSpawnLookup = new Dictionary<string, AttackSpawn>();
+        foreach(AttackSpawn spawn in AttackSpawns)
+            _attackSpawnLookup.Add(spawn.Name, spawn);
+    }
+
+    void InitCooldowns(){
+        _timesFired = new int[Projectiles.Length];
+
+        _projectileCooldowns = new float[Projectiles.Length];
+        for(int i = 0; i < Projectiles.Length; i++){
+            _projectileCooldowns[i] = Projectiles[i].Predelay;
+        }
+    }
+
+    void Update(){
+        for(int i = 0; i < Projectiles.Length; i++)
+            _projectileCooldowns[i] -= Time.deltaTime;
+        UpdateAttackSpawns();
+        UpdateProjectiles();
+    }
+
+    void UpdateAttackSpawns(){
+
+    }
+
+    async void UpdateProjectiles(){
+        if(!Triggered())
+            return;
+        for(int i = 0; i < Projectiles.Length; i++)
+            if(_projectileCooldowns[i] <= 0 && _timesFired[i]<Projectiles[i].Repeats){
+                SpawnProjectile(Projectiles[i]);
+                _projectileCooldowns[i] += Projectiles[i].TimeBetweenShots;
+                _timesFired[i]++;
+            }
+            
+    }
+
+    void SpawnProjectile(AttackInfo proj){
+        foreach(string spawn in proj.AttackSpawnNames){
+            GameObject shot = Instantiate(proj.Shot, _attackSpawnLookup[spawn].SpawnTransform);
+            // TODO: deparent, variable destroy times
+            Destroy(shot, 5);
+        }
+
+    }
+
+    public bool Triggered(){
+        // TODO: trigger logic
+        return true;
+    }
+}
+
+[System.Serializable]
+public class AttackSpawn{
+    public string Name;
+    public Transform SpawnTransform;
+    public bool TrackPlayer;
+    public float PlayerTrackStrength;
+}
+
+[System.Serializable]
+public class AttackInfo{
+    public GameObject Shot;
+    public string[] AttackSpawnNames;
+    public float Predelay;
+    public int Repeats;
+    public float TimeBetweenShots;
+}
+
+[System.Serializable]
+public class AttackTrigger{
+    public bool AlwaysTriggered;
+    public bool Proximity;
+    public float MinTriggerRadius;
+    public float MaxTriggerRadius;
+    public bool Triggered(float distance){
+        // TODO: huge bug with triggering attacks
+        if(AlwaysTriggered)
+            return true;
+        if(Proximity)
+            return MinTriggerRadius <= distance && distance <= MaxTriggerRadius;
+        return false;
+    }
+}
