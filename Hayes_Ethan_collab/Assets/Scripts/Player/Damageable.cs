2bca2bd7a1b9e2576e0484649b1870fd528bc026
ethangilmore <ethanmckgilmore@gmail.com>
2022-04-06 16:58:54 -0700

    Added heal for damageables

--- Hayes_Ethan_collab/Assets/Scripts/Player/Damageable.cs
+++ Hayes_Ethan_collab/Assets/Scripts/Player/Damageable.cs
@@ -7,6 +7,7 @@ public class Damageable : MonoBehaviour
     [Space(15)]
     [Header("Health")]
     public float Health;
+    private float _health;
     [Space(5)]
     public AudioClip DamageSound;
 
@@ -19,14 +20,26 @@ public class Damageable : MonoBehaviour
     public delegate void DeathHandler();
     public event DeathHandler OnDeath;
 
+    public virtual void Awake(){
+        _health = Health;
+    }
+
     public bool Damage(float damage)
     {
-        if(Invincible) return false;
-        Health -= damage;
+        if(Invincible || damage <= 0) return false;
+        _health -= damage;
         if(OnDamage != null)
             OnDamage();
-        if (Health <= 0 && OnDeath != null)
+        if (_health <= 0 && OnDeath != null)
             OnDeath();
+        return true;
+    }
+
+    public bool Heal(float amount){
+        if(amount <= 0 ) return false;
+        _health += amount;
+        if(_health > Health)
+            _health = Health;
         return true;
     }
 }
